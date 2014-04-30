5bdc82c8113859eb823782df110fd2663ecfcca1
devkook <devinkook@gmail.com>
2014-04-30 15:59:23 +0900

    Update App.java

--- src/main/java/devkook/study/java2cassandra/App.java
+++ src/main/java/devkook/study/java2cassandra/App.java
@@ -10,30 +10,31 @@ import me.prettyprint.hector.api.mutation.Mutator;
 import me.prettyprint.hector.api.query.ColumnQuery;
 import me.prettyprint.hector.api.query.QueryResult;
 
-
 public class App {
-
-
     private final Cluster cluster;
     private final Keyspace keyspace;
 
     private final StringSerializer strSerializer;
 
-    public App(String host, String cluster, String keyspace) {
-        this.cluster = HFactory.getOrCreateCluster(cluster, hostInfomation);
-        this.keyspace = HFactory.createKeyspace(keyspace, this.cluster);
+    /**
+     *
+     * @param hostIpPort
+     * @param clusterName
+     * @param keyspaceName
+     */
+    public App(String hostIpPort, String clusterName, String keyspaceName) {
+        this.cluster = HFactory.getOrCreateCluster(clusterName, hostIpPort);
+        this.keyspace = HFactory.createKeyspace(keyspaceName, this.cluster);
 
         this.strSerializer = StringSerializer.get();
-
-
     }
 
     public MutationResult insert(String columnfamilyName, String columnName, String rowKey, String value) {
-        Mutator<String> mutator = this.mutator = HFactory.createMutator(keyspace, this.strSerializer);
-        return mutator.insert(rowKey, columnfamilyName, HFactory.createStringColumn(columnName, value));
+        Mutator<String> m = HFactory.createMutator(keyspace, this.strSerializer);
+        return m.insert(rowKey, columnfamilyName, HFactory.createStringColumn(columnName, value));
     }
 
-    public String select(String columnfamilyName, String columnName, String rowKey){
+    public String select(String columnfamilyName, String columnName, String rowKey) {
         ColumnQuery<String, String, String> columnQuery
                 = HFactory.createColumnQuery(keyspace, strSerializer, strSerializer, strSerializer);
 
@@ -47,9 +48,40 @@ public class App {
         return result;
     }
 
-    public void delete(String columnfamilyName, String columnName, String rowKey){
-        Mutator<String> mutator = this.mutator = HFactory.createMutator(keyspace, this.strSerializer);
-        mutator.delete(rowKey, columnfamilyName, columnName, stringSerializer);
+    public void delete(String columnfamilyName, String columnName, String rowKey) {
+        Mutator<String> m = HFactory.createMutator(keyspace, this.strSerializer);
+        m.delete(rowKey, columnfamilyName, columnName, this.strSerializer);
+    }
+
+    public static void main(String[] args) {
+
+        if(args.length != 6) {
+            System.err.println("ex) App localhost:9160 clusterName keyspaceName columnfamilyName columnName loop_count");
+            return;
+        }
+
+        String hostIpPort = args[0];
+        String clusterName = args[1];
+        String keyspaceName = args[2];
+
+        App app = new App(hostIpPort, clusterName, keyspaceName);
+
+        String columnfamilyName = args[3];
+        String columnName = args[4];
+        long loopcount = Long.parseLong(args[5]);
+
+        String rowKey;
+        String value;
+        RandomGenerator r = new RandomGenerator();
+        for (long i = 0; i < loopcount; i++) {
+            rowKey = r.ranRowkey();
+            value = r.ranString();
+            app.insert(columnfamilyName, columnName, rowKey, value);
+            System.out.print(".");
+        }
+        System.out.println("");
+        System.out.println("END:"+ loopcount);
 
     }
 }
+
